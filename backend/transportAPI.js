cb5bb71641503de9352284ad74c5d1e8a122a12e
Yves Bonjour <yves.bonjour@rethinc.ch>
2021-04-26 16:19:48 +0200

    Consider walk time when showing time tables for stations

--- backend/transportAPI.js
+++ backend/transportAPI.js
@@ -1,25 +1,37 @@
 import axios from 'axios'
 
 const stationBoardConfigurations = {
-  "weissenbuehl": {
+  'weissenbuehl': {
     stationId: 8595142,
-    limit: 5
+    limit: 5,
+    firstIgnoredMinutes: 2,
+    estimatedConnectionsPerMinute: 1
   },
-  "wander": {
+  'wander': {
     stationId: 8589993,
-    limit: 5
+    limit: 5,
+    firstIgnoredMinutes: 5,
+    estimatedConnectionsPerMinute: 3
   }
 }
 
 export const fetchNextDepartures = async (stationName) => {
   const config = stationBoardConfigurations[stationName]
-  const url = `https://transport.opendata.ch/v1/stationboard?id=${config.stationId}&limit=${config.limit}`
+  const limit = config.firstIgnoredMinutes * config.estimatedConnectionsPerMinute + config.limit
+  const nowTimestamp = new Date().valueOf() / 1000
+
+  const url = `https://transport.opendata.ch/v1/stationboard?id=${config.stationId}&limit=${limit}`
   const response = await axios.get(url)
-  return response.data.stationboard.map((stationboard) => {
-    return {
-      departure: stationboard.stop.departureTimestamp,
-      delay: stationboard.stop.delay,
-      to: stationboard.to
-    }
-  })
+  return response.data.stationboard
+    .filter((stationboard) => {
+      return nowTimestamp + config.firstIgnoredMinutes * 60 <= stationboard.stop.departureTimestamp
+    })
+    .slice(0, config.limit)
+    .map((stationboard) => {
+      return {
+        departure: stationboard.stop.departureTimestamp,
+        delay: stationboard.stop.delay,
+        to: stationboard.to
+      }
+    })
 }
