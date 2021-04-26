cf071ecaeaf8918a5bded3245d1573b86fa34e80
Yves Bonjour <yves.bonjour@rethinc.ch>
2021-04-26 14:58:20 +0200

    Shows minutes till departure

--- web/src/components/departures/Departure.vue
+++ web/src/components/departures/Departure.vue
@@ -1,5 +1,5 @@
 <template>
-  {{ formatTime(departure.departure) }} - {{ departure.to }}
+  <span class="minutes">{{ formatMinutes(departure.departure) }}</span> ({{ formatTime(departure.departure) }}) - {{ departure.to }}
 </template>
 
 <script lang="ts">
@@ -9,20 +9,31 @@ import { PropType } from 'vue'
 export default {
   props: {
     departure: {
-      type:  Object as PropType<DepartureModel>,
+      type: Object as PropType<DepartureModel>,
       required: true
     }
   },
   methods: {
     formatTime: (timestamp: number): string => {
       const zeroPadding = (n: number): string => {
-          return `${n < 10 ? '0': ''}${n}`
+        return `${n < 10 ? '0' : ''}${n}`
       }
 
       const date = new Date(timestamp * 1000)
 
       return `${zeroPadding(date.getHours())}:${zeroPadding(date.getMinutes())}`
+    },
+    formatMinutes: (timestamp: number): string => {
+      const now = new Date()
+      const differenceMs = timestamp - (now.valueOf() / 1000)
+      return `${Math.floor(differenceMs / 60)}\``
     }
   }
 }
 </script>
+
+<style>
+  .minutes {
+    font-weight: bolder;
+  }
+</style>
