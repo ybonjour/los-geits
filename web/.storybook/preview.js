b3340b811da03660449b8329a6d97e6a93b0a062
Yves Bonjour <yves.bonjour@rethinc.ch>
2022-01-12 14:15:25 +0100

    Supports fontsawesome in storybook

--- web/.storybook/preview.js
+++ web/.storybook/preview.js
@@ -1,9 +1,17 @@
+import {app} from '@storybook/vue3'
+import {FontAwesomeIcon} from '@fortawesome/vue-fontawesome'
+import {library} from '@fortawesome/fontawesome-svg-core'
+import {fas} from '@fortawesome/free-solid-svg-icons'
+
+library.add(fas)
+app.component('font-awesome-icon', FontAwesomeIcon)
+
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
+  actions: {argTypesRegex: '^on[A-Z].*'},
   controls: {
     matchers: {
       color: /(background|color)$/i,
-      date: /Date$/,
-    },
-  },
+      date: /Date$/
+    }
+  }
 }
