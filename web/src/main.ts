f0bada9101f315198d65cdcc7735e091b3fc4731
Yves Bonjour <yves.bonjour@rethinc.ch>
2022-01-12 14:16:04 +0100

    Adds all fontawesome solid icons

--- web/src/main.ts
+++ web/src/main.ts
@@ -1,19 +1,13 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import { router } from '@/router'
-import { library} from '@fortawesome/fontawesome-svg-core'
-import { faSubway } from "@fortawesome/free-solid-svg-icons";
-import { faCar } from "@fortawesome/free-solid-svg-icons";
-import { faBicycle } from "@fortawesome/free-solid-svg-icons";
-import { faWalking } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fas } from '@fortawesome/free-solid-svg-icons'
 
-library.add(faSubway)
-library.add(faCar)
-library.add(faBicycle)
-library.add(faWalking)
+library.add(fas)
+const vueApp = createApp(App)
+vueApp.component('font-awesome-icon', FontAwesomeIcon)
+vueApp.use(router)
+vueApp.mount('#app')
 
-const app = createApp(App)
-app.component("font-awesome-icon", FontAwesomeIcon)
-app.use(router)
-app.mount('#app')
